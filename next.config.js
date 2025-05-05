767258e2d51c8ba6b438dbf9d1984c985d986e22
meganvchai <mvchai@andrew.cmu.edu>
2025-05-05 14:53:10 -0700

    chore: update Next.js config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/pixd-a3',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
